f5878e54ab3d9c94fdc75caf99d3aed8f9d09316
Deanne McPherson <deanne.mcpherson@gmail.com>
2014-06-09 03:21:13 -0400

    updating to Python 3

--- sessions/2014/basics/variableList.py
+++ sessions/2014/basics/variableList.py
@@ -13,4 +13,4 @@ second_name = None
 # this code should write out the filled arrays and the second name in the names list (Eric).
 print(numbers)
 print(strings)
-print("The second name on the names list is %s" % second_name)
+print("The second name on the names list is %s" % second_name)s
