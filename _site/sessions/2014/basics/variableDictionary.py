4d525537caf04010d7650c6f2bd7ad0ccf445536
Carl Pearson <cap10@ufl.edu>
2014-06-09 06:46:12 -0400

    Build site at 2014-06-09 10:46:12 UTC

--- _site/sessions/2014/basics/variableDictionary.py
+++ _site/sessions/2014/basics/variableDictionary.py
@@ -12,6 +12,6 @@ phonebook = {
 
 # testing code
 if "Jake" in phonebook:
-    print "Jake is listed in the phonebook."
+    print ("Jake is listed in the phonebook.")
 if "Jill" not in phonebook:
-    print "Jill is not listed in the phonebook."
+    print ("Jill is not listed in the phonebook.")
