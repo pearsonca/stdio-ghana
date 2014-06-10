dae38b3cd325100f2464fae566e7dc5b7365737c
seifu <chondesj@Gmail.com>
2014-06-10 15:33:26 +0000

    Create classAct.py

---
+++ sessions/2014/advanced/classAct.py
@@ -0,0 +1,15 @@
+class Employee:
+    'Common base for all employees'
+
+    def __init__(self, name, salary = 20):
+        self.name = name
+        self.salary = salary
+
+    def displayEmployee(self):
+        print "Name: ", self.name, ", Salary: ", self.salary
+
+if __name__ == "__main__":
+    emp1 = Employee('name')
+    emp1.displayEmployee()
+    emp2 = Employee('name2', 25)
+    emp2.displayEmployee()
