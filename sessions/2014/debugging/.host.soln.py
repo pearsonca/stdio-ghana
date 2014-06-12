fe6ae022f556d95a2cfad778f9d93d519711453d
Live session user <kubuntu@kubuntu.(none)>
2014-06-12 03:39:11 +0000

    End of debugging

--- sessions/2014/debugging/.host.soln.py
+++ sessions/2014/debugging/.host.soln.py
@@ -7,12 +7,14 @@ import sys
 # otherwise, the customer should be added to a waiting list
 
 # Original Table status -- DO NOT CHANGE!
-tables = [None for r in range(10)]   # List of tables in the restaurant
+######## Runtime Error 2: Range off-by-one, should ask for 10 objects
+tables = [None for r in range(0,9)]   # List of tables in the restaurant
 tables[0] = {"max":4,"status":"occupied","number":0}
 tables[1] = {"max":4,"status":"occupied","number":1}
 tables[2] = {"max":2,"status":"occupied","number":2}
 tables[3] = {"max":4,"status":"occupied","number":3}
-tables[4] = {"max":2,"status":"available","number":4}
+######## Syntax Error 1: Need comma between each item
+tables[4] = {"max":2,"status":"available""number":4}
 tables[5] = {"max":4,"status":"occupied","number":5}
 tables[6] = {"max":4,"status":"available","number":6}
 tables[7] = {"max":2,"status":"occupied","number":7}
@@ -23,29 +25,31 @@ tables[9] = {"max":2,"status":"occupied","number":9}
 def CheckTables():
 	avail = []
 	for place in tables:
-		#Logic Error 1: Should check that status available
+		#########Logic Error 1: Should check that status available
 		if place["max"] - size in (0,1) and place["status"] is "occupied":
 			avail.append(place["number"])
 	return avail
 
 # This Dictionary will keep track of waiting patrons
-wait = {}
+######## Runtime Error 1: Dictionary should use curly braces {}
+wait = []
 
 # Create a loop which allows multiple visitors to request tables
 # A party size of zero should indicate an empty line
 # *Interesting addition: Have a table randomly made available each iteration*
 
-while 1:
+######## Syntax Error 2: While statement should end with colon, not semicolon
+while 1;
 	# Ask the user how many are in their party
 	size = input("\nAkwaba! How many people are in your party? ")
 	
-	# Validation Error 1: Don't handle the exception of a user entering a non-number
+	######### Validation Error 1: Don't handle the exception of a user entering a non-number
 	size = int(size)
 	
 	# Check if this party will fit with any table availability
-	########################## MODIFY CODE HERE!! #################################
 	# Discern availability of tables inside a function
-	avail_tables = CheckTables()
+	######## Runtime Error 3: Function call must include parentheses
+	avail_tables = CheckTables
 			
 	# If yes - let them know they will be seated
 	# If no - let them know how long they must wait
@@ -55,7 +59,7 @@ while 1:
 		print("Right this way. You will be seated at table %d." % your_table)
 		tables[your_table]["status"] = "occupied"
 	elif size <= 0 or size > max([sz["max"] for sz in tables]):
-		#Logic Error 2: Should have checked size < 0; program cannot exit cleanly
+		#########Logic Error 2: Should have checked size < 0; program cannot exit cleanly
 		print("I'm sorry, our tables do not hold parties of that size.")
 	elif size == 0:
 		print("\nI see the line is now empty.  We are closing for the evening.\n")
@@ -63,6 +67,9 @@ while 1:
 	else:
 		# Only add party to the wait list if that name is not already on it.
 		w = input("You will need to wait for a table. What is your name? ")
+		#########Validation Error 2: Dangerous to allow eval() to execute arbitrary code
+		try: eval(w)
+		except NameError: None
 	
 		if w not in wait.keys():
 			wait[w] = size
--- sessions/2014/debugging/hostEX.py
+++ sessions/2014/debugging/hostEX.py
@@ -7,12 +7,12 @@ import sys
 # otherwise, the customer should be added to a waiting list
 
 # Original Table status -- DO NOT CHANGE!
-tables = [None for r in range(10)]   # List of tables in the restaurant
+tables = [None for r in range(0,9)]   # List of tables in the restaurant
 tables[0] = {"max":4,"status":"occupied","number":0}
 tables[1] = {"max":4,"status":"occupied","number":1}
 tables[2] = {"max":2,"status":"occupied","number":2}
 tables[3] = {"max":4,"status":"occupied","number":3}
-tables[4] = {"max":2,"status":"available","number":4}
+tables[4] = {"max":2,"status":"available""number":4}
 tables[5] = {"max":4,"status":"occupied","number":5}
 tables[6] = {"max":4,"status":"available","number":6}
 tables[7] = {"max":2,"status":"occupied","number":7}
@@ -23,32 +23,26 @@ tables[9] = {"max":2,"status":"occupied","number":9}
 def CheckTables():
 	avail = []
 	for place in tables:
-		if place["max"] - size in (0,1) and place["status"] is "available":
+		if place["max"] - size in (0,1) and place["status"] is "occupied":
 			avail.append(place["number"])
 	return avail
 
 # This Dictionary will keep track of waiting patrons
-wait = {}
+wait = []
 
 # Create a loop which allows multiple visitors to request tables
 # A party size of zero should indicate an empty line
 # *Interesting addition: Have a table randomly made available each iteration*
 
-while 1:
+while 1;
 	# Ask the user how many are in their party
 	size = input("\nAkwaba! How many people are in your party? ")
 	
-	# Handle the exception of a user entering a non-number
-	try: 
-		size = int(size)
-	except ValueError:
-		print("OK Joker, try again when you can enter a real number.\nGoodbye!")
-		sys.exit(0)	
+	size = int(size)
 	
 	# Check if this party will fit with any table availability
-	########################## MODIFY CODE HERE!! #################################
 	# Discern availability of tables inside a function
-	avail_tables = CheckTables()
+	avail_tables = CheckTables
 			
 	# If yes - let them know they will be seated
 	# If no - let them know how long they must wait
@@ -57,7 +51,7 @@ while 1:
 		your_table = avail_tables.pop()
 		print("Right this way. You will be seated at table %d." % your_table)
 		tables[your_table]["status"] = "occupied"
-	elif size < 0 or size > max([sz["max"] for sz in tables]):
+	elif size <= 0 or size > max([sz["max"] for sz in tables]):
 		print("I'm sorry, our tables do not hold parties of that size.")
 	elif size == 0:
 		print("\nI see the line is now empty.  We are closing for the evening.\n")
@@ -65,6 +59,8 @@ while 1:
 	else:
 		# Only add party to the wait list if that name is not already on it.
 		w = input("You will need to wait for a table. What is your name? ")
+		try: eval(w)
+		except NameError: None
 	
 		if w not in wait.keys():
 			wait[w] = size
